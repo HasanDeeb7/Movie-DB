43d59272c29577f97fd08aeaa4c67b0319952b0d
Hasan Deeb <h.deeb712@gmail.com>
2023-09-21 20:19:44 +0300

    created 2 routes test display 'ok' as message, time display current time as hh:ss format

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+
 var express = require("express");
 var app = express();
 app.listen(5000);
-app.get("/", (req, res) => {
-  res.send("OK");
+app.get("/test", (req, res) => {
+  res.send({status: 200, message:'ok'});
+});
+app.get("/time", (req, res) => {
+  const date = new Date()
+  const hours = date.getHours()
+  const seconds = date.getSeconds()
+  res.status(200).json({status: 200, message:`${hours} : ${seconds}`});
 });
